b033fd70cbdc00f2427b1e9483de4db49bc8c210
JackGHudUni <u1759958@unimail.hud.ac.uk>
2019-02-18 14:32:16 +0000

    jack3

--- Assets/Plugins/GitHub/Editor/ExtensionLoader.cs
+++ Assets/Plugins/GitHub/Editor/ExtensionLoader.cs
@@ -30,18 +30,9 @@ namespace GitHub.Unity
         private static string[] assemblies20 = { "System.Threading.dll", "AsyncBridge.Net35.dll", "ReadOnlyCollectionsInterfaces.dll", "GitHub.Api.dll", "GitHub.Unity.dll" };
         private static string[] assemblies45 = { "GitHub.Api.45.dll", "GitHub.Unity.45.dll" };
 
-<<<<<<< HEAD
         static ExtensionLoader()
         {
             if (Environment.GetEnvironmentVariable("GITHUB_UNITY_DISABLE") == "1")
-=======
-        private const string GITHUB_UNITY_DISABLE = "GITHUB_UNITY_DISABLE";
-        private static bool IsDisabled { get { return Environment.GetEnvironmentVariable(GITHUB_UNITY_DISABLE) == "1"; } }
-
-        static ExtensionLoader()
-        {
-            if (IsDisabled)
->>>>>>> 1aa4b80b0ee0fe86ac1c79dcf1fe52b857d734fa
             {
                 return;
             }
