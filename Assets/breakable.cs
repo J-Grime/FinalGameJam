56e513506359b14c0b92de3178c74cf72157683a
Aliggins <U1760724@unimail.hud.ac.uk>
2019-02-18 14:28:14 +0000

    Commit Alwx 2

---
+++ Assets/breakable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class breakable : MonoBehaviour {
+
+    public float hitPoints;
+    public GameObject drop;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void damage(float hitDamage, GameObject player)
+    {
+        hitPoints -= hitDamage;
+        Debug.Log("hit");
+     //   player.GetComponent<InventoryManager>().itemPickup(drop);
+    }
+}
