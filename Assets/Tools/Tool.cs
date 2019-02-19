4e60c3cbffe0dea199207eaa9ae22b676104a1c8
Aliggins <U1760724@unimail.hud.ac.uk>
2019-02-19 11:05:40 +0000

    day 2 commit 1

---
+++ Assets/Tools/Tool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour {
+    public string toolname;
+	// Use this for initialization
+	void Start () {
+        //toolname = "axe";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Tools/Torch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : Tool{
+    
+	// Use this for initialization
+	void Start () {
+        toolname = "Torch";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
