9db31f01a1aeb6cf044fe7eba825f52203a55bd8
Aliggins <U1760724@unimail.hud.ac.uk>
2019-02-19 16:31:12 +0000

    fixed bug

--- Assets/TomStuff/treeGen.cs
+++ Assets/TomStuff/treeGen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 public class treeGen : MonoBehaviour
-{ 
+{
     public float timer = 0;
     public float timediff = 0.9f;
     public Tree baseTree;
@@ -20,25 +20,26 @@ public class treeGen : MonoBehaviour
         float posZ = transform.position.z + Random.Range(-radius, radius);
         Vector3 randPoint = new Vector3(posX, transform.position.y, posZ);
 
-<<<<<<< HEAD
+
 
         if (timer > timediff)
-        { 
+        {
             if (Physics.Raycast(randPoint, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
-=======
-        if (Physics.Raycast(randPoint, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
-        { 
-            if (hit.collider.gameObject.tag == "ground")
->>>>>>> 9689a3bd6c182d5904669613e2a38046ad9869c2
-            {
-                if (hit.collider.gameObject.tag == "ground")
-                {
-                    newTree = Instantiate(baseTree, new Vector3(hit.point.x, hit.point.y, hit.point.z), transform.rotation);
-                    //newTree.transform.parent = transform;
 
+                if (Physics.Raycast(randPoint, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.gameObject.tag == "ground")
+
+                    {
+                        if (hit.collider.gameObject.tag == "ground")
+                        {
+                            newTree = Instantiate(baseTree, new Vector3(hit.point.x, hit.point.y, hit.point.z), transform.rotation);
+                            //newTree.transform.parent = transform;
+
+                        }
+                    }
+                    timer = 0;
                 }
-            }
-            timer = 0;
         }
     }
 }
