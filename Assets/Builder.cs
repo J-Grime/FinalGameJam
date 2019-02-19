2bd8ebf8238b0e3f82ef544f62bf9a7ca4e5ff12
JackGHudUni <u1759958@unimail.hud.ac.uk>
2019-02-19 15:02:24 +0000

    jack7

---
+++ Assets/Builder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour {
+    [SerializeField] public GameObject building;
+    [SerializeField] public int wood;
+    [SerializeField] public int stone;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
