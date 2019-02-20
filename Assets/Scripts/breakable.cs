c154325fc9853bad25b65efc1c89029970e54ac5
4myr0se <32360145+4myr0se@users.noreply.github.com>
2019-02-20 14:24:39 +0000

    added aspects of interact.cs to the breakable script
    may be able to remove interact.cs later..

--- Assets/Scripts/breakable.cs
+++ Assets/Scripts/breakable.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public abstract class breakable : health {
 
     public GameObject drop;
-
+    public float distanceAllowed;
+    public Material outlineMaterial;
+    public Material normalMaterial;
+    public MeshRenderer meshRenderer;
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,5 +24,23 @@ public abstract class breakable : health {
         hitPoints -= hitDamage;
         Debug.Log("hit");
         player.GetComponent<InventoryManager>().itemPickup(drop);
+    }
+
+    void OnMouseEnter()
+    {
+        //IF Player is less than X units away:
+        //if (Vector3.Distance(transform.position, GameController.Instance.GetPlayer().transform.position) <= distanceAllowed)
+
+        print("Mouse on breakable object");
+        meshRenderer.material = outlineMaterial;
+        
+        //}
+
+    }
+
+    void OnMouseExit()
+    {
+        print("Mouse moved off of breakable object");
+        meshRenderer.material = normalMaterial;
     }
 }
