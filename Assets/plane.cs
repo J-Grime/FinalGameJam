eaeb5c32cb3e6c616eca81cf27d1b4bb5a19c78b
JackGHudUni <u1759958@unimail.hud.ac.uk>
2019-02-20 11:24:46 +0000

    commit10jack

---
+++ Assets/plane.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plane : MonoBehaviour {
+    Rigidbody RB;
+    public int speed;
+    public Sun SunScript;
+    GameObject script;
+	// Use this for initialization
+	void Start () {
+        script.SetActive(false);
+        script.GetComponent<plane>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlaneFly();
+	}
+
+    void PlaneFly() {
+        transform.Translate(Vector3.right * Time.deltaTime*speed);
+    }
+}
+
+
+
