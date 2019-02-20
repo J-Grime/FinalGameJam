5c7aa60d3ff017c2a0912190566853d68827ac63
JackGHudUni <u1759958@unimail.hud.ac.uk>
2019-02-20 17:32:28 +0000

    jack15

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,13 +9,45 @@ public class Game : MonoBehaviour {
     GameObject[] Flare;
     int score;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         campfires = GameObject.FindGameObjectsWithTag("campfire");
-        Debug.Log("length is  "+campfires.Length);
+        SOS = GameObject.FindGameObjectsWithTag("SOS");
+        Radio = GameObject.FindGameObjectsWithTag("radio");
+        Flare = GameObject.FindGameObjectsWithTag("Flare");
+
+        if (campfires.Length >= 3)
+        { score += 9; }
+        else
+        { score += campfires.Length * 3; }
+        
+
+
+
+
+        if (SOS.Length >= 3)
+        { score += 15; }
+        else { score += SOS.Length * 5; }
+
+
+
+
+
+
+
+
+
+
+
+        Debug.Log("score is     "+ score);
+
+
+        if (score >= 100)
+        { Debug.Log("Rescued"); }
+        gameObject.SetActive(false);
 	}
 }
