7e95f6cbafc549f831d9bc3329348f1722d36a19
JackGHudUni <u1759958@unimail.hud.ac.uk>
2019-02-21 14:00:26 +0000

    commit19

---
+++ Assets/Scripts/catchPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catchPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "player")
+        {
+
+            other.GetComponent<playerHealthManager>().damage(15);
+            StartCoroutine(wait());
+        }
+    }
+    IEnumerator wait()
+
+    {
+        yield return new WaitForSeconds(5);
+    }
+}
