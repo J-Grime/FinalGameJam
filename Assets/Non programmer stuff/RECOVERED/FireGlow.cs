f0cba3d820c5c185001ea74904082f56b8a5dadf
Aliggins <U1760724@unimail.hud.ac.uk>
2019-02-21 13:19:26 +0000

    fire

---
+++ Assets/Non programmer stuff/RECOVERED/FireGlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGlow : MonoBehaviour {
+    Light thislight;
+    float rand;
+    
+    bool direction = true;
+	// Use this for initialization
+	void Start () {
+        thislight = this.GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       if (direction == true){thislight.intensity++; rand = Random.Range(1,3); }
+        if (direction == false) { thislight.intensity--; rand = Random.Range(1,3); }
+
+        if (thislight.intensity > 9+rand) { direction = false; }
+        if (thislight.intensity < 8-rand) { direction = true; }
+    }
+}
