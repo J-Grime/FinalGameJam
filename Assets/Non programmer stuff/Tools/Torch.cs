50ed4daa45a095f3b4392e2709a39dd7e734d7a0
Aliggins <U1760724@unimail.hud.ac.uk>
2019-02-21 14:43:11 +0000

    thurs2

--- Assets/Non programmer stuff/Tools/Torch.cs
+++ Assets/Non programmer stuff/Tools/Torch.cs
@@ -7,6 +7,9 @@ public class Torch : Tool{
 	// Use this for initialization
 	void Start () {
         toolname = "Torch";
+        reqMaterials.Add("wood", 20);
+        reqMaterials.Add("rock", 0);
+        reqMaterials.Add("gunpowder", 0);
     }
 	
 	// Update is called once per frame
