c71effab74da7e5794ad62824b1379e2f2c07571
JackGHudUni <u1759958@unimail.hud.ac.uk>
2019-02-22 04:19:40 +0000

    jack24

---
+++ Assets/RandomCase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCase : MonoBehaviour {
+    public GameObject thisCase;
+    public GameObject openedCase;
+    public GameObject[] suitcase;
+    int num;
+	// Use this for initialization
+	void Start () {
+        suitcase = GameObject.FindGameObjectsWithTag("suitcase");
+        Debug.Log("amount of cases"+suitcase.Length);
+        num = RandomNumber(0, suitcase.Length);
+        Debug.Log(num);
+        suitcase[num].SetActive(false);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    public int RandomNumber(int min, int max)
+    {
+        Random random = new Random();
+        return Random.Range(min, max);
+    }
+}
